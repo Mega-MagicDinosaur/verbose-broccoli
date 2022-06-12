b913466d763403539218cc1165f3bf262ed9024c
Alessandro Somigli <alesomirodari@gmail.com>
2022-06-12 14:33:32 +0200

    changes on main frontend page added links

--- home/urls.py
+++ home/urls.py
@@ -6,6 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.index_page, name='index_page'),
     path('<int:pk>', views.home_page, name='home_page'),
+    path('<int:pk>/radar_view', views.radar_view_page, name='radar_view_page'),
+    path('<int:pk>/company_list', views.company_list_page, name='company_list_page'),
 ]
 # static is to use only in debug!!
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- home/views.py
+++ home/views.py
@@ -1,17 +1,37 @@
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-
 # Create your views here.
+from django.urls import reverse
+
+
 def index_page(request):
     context = {}
     return render(request, 'home/index.html', context)
 
 
 def home_page(request, pk):
-    user = User.objects.get(id=pk)
-    context = {'user': user}
     if request.user.is_authenticated and request.user.id == pk:
+        return redirect('radar_view_page', pk)
+    else:
+        return redirect('login_page')
+
+
+def radar_view_page(request, pk):
+    if request.user.is_authenticated and request.user.id == pk:
+        user = User.objects.get(id=pk)
+        context = {'user': user}
         return render(request, 'home/radar_view.html', context)
+    else:
+        return redirect('login_page')
+
+
+def company_list_page(request, pk):
+    if request.user.is_authenticated and request.user.id == pk:
+        user = User.objects.get(id=pk)
+        context = {'user': user}
+        return render(request, 'home/company_list.html', context)
     else:
         return redirect('login_page')
